a5e776369be18cbb00f8dd3fc5bc6c0d6bf92a6c
VISHAL <vishalvkamble95@gmail.com>
2023-06-09 17:28:10 +0530

    user API

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,76 @@
 const router = require("express").Router();
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {});
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE METHOD
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("user Has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get User
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER STATS
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.getFullYear(date.getFullYear() - 1));
+
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 function verifyToken(req, res, next) {
   const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) res.status(403).json("Token is not Valid");
       req.user = user;
@@ -12,14 +13,29 @@ function verifyToken(req, res, next) {
     return res.status(401).json("you are not Authenticated");
   }
 }
+
 const verifyTokenAndAuthorization = async (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      res.status(403).jason("You are not allowed to do that!");
+      res.status(403).json("You are not allowed to do that!");
     }
   });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = async (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
